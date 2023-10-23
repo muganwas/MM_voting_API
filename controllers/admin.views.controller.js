8f3a34b4fde70008326e8d41f69f6b70ad9f0ea4
Muganwa <stevenmuganwa@live.com>
2023-10-23 11:47:03 +0300

    doc and livereload refactor

--- controllers/admin.views.controller.js
+++ controllers/admin.views.controller.js
@@ -8,6 +8,7 @@ const { enums: { VALIDATION_ERROR, UNAUTHORIZED_ERROR }, messages: { VALIDATION_
 router.get('/', function (req, res, next) {
     res.sendFile(path.join(__dirname, '../views/Admin/index.html'));
 });
+
 router.get('/dashboard', async function (req, res, next) {
     if (!req.headers.authorization) return next({ name: VALIDATION_ERROR, message: VALIDATION_MESSAGE }, req, res, next);
     if (!await validateFirebaseAdmin(req.headers.authorization)) return next({ name: UNAUTHORIZED_ERROR, message: UNAUTHORIZED_MESSAGE }, req, res, next);
